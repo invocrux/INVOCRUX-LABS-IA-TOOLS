4452af2ecfdb5cf8f676c1cef12b278e86f4be57
Roberto Jose De La Cruz Padilla <101188386+invocrux@users.noreply.github.com>
2026-02-07 15:41:28 -0500

    chore: Add TypeScript interfaces

---
+++ src/interfaces/interface.ts
@@ -0,0 +1,80 @@
+export interface IToolCall {
+  type: string;
+  function: IFunctionDefinition;
+}
+
+export interface IFunctionDefinition {
+  name: string;
+  description: string;
+  parameters: IParameters;
+}
+
+export interface IParameters {
+  type: string;
+  properties: {
+    [key: string]: {
+      type: string;
+      description: string;
+    };
+  };
+  required: string[];
+}
+
+export interface ILLMMessage {
+    model: string;
+    messages: IMessagesInput[];
+    tools: IToolCall[];
+    tool_choice: string;
+}
+
+export interface IMessagesInput {
+    role: "user" | "assistant" | "tool";
+    content?: string;
+    tool_call_id?: string;
+    name?: string;
+    tool_calls?: ToolCall[];
+}
+
+export interface ILLMResponse {
+    id:                 string;
+    object:             string;
+    created:            number;
+    model:              string;
+    choices:            Choice[];
+    usage:              Usage;
+    stats:              Stats;
+    system_fingerprint: string;
+}
+
+export interface Choice {
+    index:         number;
+    message:       Message;
+    logprobs:      null;
+    finish_reason: string;
+}
+
+export interface Message {
+    role:       string;
+    content:    string;
+    tool_calls: ToolCall[];
+}
+
+export interface ToolCall {
+    type:     string;
+    id:       string;
+    function: Function;
+}
+
+export interface Function {
+    name:      string;
+    arguments: string;
+}
+
+export interface Stats {
+}
+
+export interface Usage {
+    prompt_tokens:     number;
+    completion_tokens: number;
+    total_tokens:      number;
+}
