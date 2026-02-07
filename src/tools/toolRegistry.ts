4695efacafffd4d4be2861b242f99ce6dfc1a3ef
Roberto Jose De La Cruz Padilla <101188386+invocrux@users.noreply.github.com>
2026-02-07 15:41:45 -0500

    feat: Add tool registry for dynamic tool function mapping

---
+++ src/tools/toolRegistry.ts
@@ -0,0 +1,21 @@
+import actualizarProductoEnSupa from "../functions/actualizarProducto";
+import buscarProductoEnSupa from "../functions/buscarProducto";
+import crearProductoEnSupa from "../functions/crearProducto";
+import elimnarProductoEnSupa from "../functions/eliminarProducto";
+import listarProductoEnSupa from "../functions/listarProducto";
+
+const toolRegistry: { [key: string]: (args: any) => Promise<string> } = {
+  actualizar_producto: actualizarProductoEnSupa, 
+  buscar_producto: buscarProductoEnSupa,
+  crear_producto: crearProductoEnSupa, 
+  eliminar_producto: elimnarProductoEnSupa,
+  listar_productos: listarProductoEnSupa,
+};
+
+export async function ejecutarTool(nombre: string, args: any): Promise<string> {
+  const toolFunction = toolRegistry[nombre];
+  if (!toolFunction) throw new Error(`Herramienta no encontrada: ${nombre}`);
+  return await toolFunction(args);
+}
+
+export default toolRegistry;
