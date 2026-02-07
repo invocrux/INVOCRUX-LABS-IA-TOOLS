918b0531d014279e3292d77248d9fa8147997c78
Roberto Jose De La Cruz Padilla <101188386+invocrux@users.noreply.github.com>
2026-02-07 15:41:54 -0500

    feat: Add list all products function

---
+++ src/functions/listarProducto.ts
@@ -0,0 +1,16 @@
+import cliente from "../services/supabase";
+
+async function listarProductoEnSupa(): Promise<string> {
+  const { data, error } = await cliente.from("inventario").select("*");
+
+  if (error) {
+    throw new Error("Error al listar los productos: " + error.message);
+  }
+
+  console.log("resultado de busqueda", data);
+  if (!data || data.length === 0)
+    return "No encontré ningún producto en el inventario.";
+  return JSON.stringify(data);
+}
+
+export default listarProductoEnSupa;
